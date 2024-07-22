5e55bd4fb21377d9abc33afd9a33c752c623a282
ag <ankit@mcbkair.local>
2024-07-23 03:24:38 +0530

    fix fa icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,16 @@ import '../public/MarkdownRenderer.css';
 import '../public/Themes.css';
 import '../public/about.css';
 
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowLeft, faHome, faMoon, faSun, faBook } from '@fortawesome/free-solid-svg-icons';
+// See https://github.com/FortAwesome/react-fontawesome#integrating-with-other-tools-and-frameworks
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+library.add(
+  faGithub, faArrowLeft, faHome, faMoon, faSun, faBook
+);
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
